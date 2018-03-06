1127cf28afda1c8e373af5807310897d09532020
Jason Campbell <me@jcampbell.co>
2018-03-06 08:36:47 -0700

    Add resources directory

--- main.py
+++ main.py
@@ -10,36 +10,38 @@ slack_client = SlackClient(config.slack_key)
 # Fetch your Bot's User ID
 user_list = slack_client.api_call("users.list")
 for user in user_list.get('members'):
-    if user.get('name') == "coffeebot":
+    if user.get('name') == config.bot_name:
         slack_user_id = user.get('id')
         break
 
 # Start connection
 if slack_client.rtm_connect():
-    print "Connected!"
+    print("Connected!")
 
     while True:
         for message in slack_client.rtm_read():
-	    if 'text' in message and message['text'].startswith("@covfefebot") and 'user' in message and message['user'] != slack_user_id:
-		slack_client.api_call(
-		    'files.upload',
-		    channels=message['channel'],
-		    filename='covfefe.png',
-                    file=open('covfefe.png', 'rb'),
-		    as_user=True
-		)
-		continue
+            if 'text' in message and message['text'].startswith("@covfefebot") and 'user' in message and message[
+                'user'] != slack_user_id:
+                slack_client.api_call(
+                    'files.upload',
+                    channels=message['channel'],
+                    filename='covfefe.png',
+                    file=open('resources/covfefe.png', 'rb'),
+                    as_user=True
+                )
+                continue
 
-            if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id) and 'user' in message and message['user'] != slack_user_id:
+            if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id) and 'user' in message and \
+                    message['user'] != slack_user_id:
 
-                print "Message received: %s" % json.dumps(message, indent=2)
+                print("Message received: %s" % json.dumps(message, indent=2))
 
                 if message['text'].find("hello") > -1:
                     slack_client.api_call(
                         "chat.postMessage",
-                        channel = message['channel'],
-                        text = "I am coffeebot, what is my purpose?",
-                        as_user = True
+                        channel=message['channel'],
+                        text="I am coffeebot, what is my purpose?",
+                        as_user=True
                     )
                     continue
 
@@ -48,7 +50,7 @@ if slack_client.rtm_connect():
                         'files.upload',
                         channels=message['channel'],
                         filename='ohmygod.gif',
-                        file=open('ohmygod.gif', 'rb'),
+                        file=open('resources/ohmygod.gif', 'rb'),
                         as_user=True
                     )
                     continue
