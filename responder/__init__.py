b55aa17b906db0e7f12dafcbb452ab0ea5aed2ba
Jason Campbell <me@jcampbell.co>
2018-03-12 20:40:50 -0600

    Add ayymote response

--- responder/__init__.py
+++ responder/__init__.py
@@ -1,18 +1,20 @@
+from .ayy import Ayy
+from .ayymote import Ayymote
 from .covfefe import Covfefe
 from .debug import Debug
 from .hello import Hello
 from .img import Img
 from .ip import Ip
 from .ohmygod import OhMyGod
-from .ayy import Ayy
 
 
 __all__ = [
+    'Ayy',
+    'Ayymote',
     'Covfefe',
     'Debug',
     'Hello',
     'Img',
     'Ip',
     'OhMyGod',
-    'Ayy',
 ]
